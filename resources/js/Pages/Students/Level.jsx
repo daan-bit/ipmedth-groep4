8c7205072fdcf4850f31d8ff6c92fe8a9198713b
Kasper <kaspertrouwee@gmail.com>
2022-12-05 15:56:44 +0100

    💄 Gave the text a bg with opacity

--- resources/js/Pages/Students/Level.jsx
+++ resources/js/Pages/Students/Level.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Head } from '@inertiajs/inertia-react';
-import {usePage } from "@inertiajs/inertia-react";
+import { usePage } from "@inertiajs/inertia-react";
 import AICanvas from "@/Components/Student/AICanvas";
 
 import "../../../css/pages/Students/Level.css";
@@ -9,9 +9,12 @@ export default function Sandbox() {
     const { level } = usePage().props;
     console.log(level);
     return (
-        <article className="container">
+        <article className="level__container">
             <Head title={level.name} />
-            <AICanvas id="canvas" />
+            <h2 className="level__description">{level.description}</h2>
+            <section className="canvas__container">
+                <AICanvas id="canvas" />
+            </section>
         </article>
     )
 }

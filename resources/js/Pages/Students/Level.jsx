467071f1142c02943057d5613a771703bbc386b2
Kasper <kaspertrouwee@gmail.com>
2022-12-07 12:45:01 +0100

    🚚 added colors.ccs to app.css

--- resources/js/Pages/Students/Level.jsx
+++ resources/js/Pages/Students/Level.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { Head } from '@inertiajs/inertia-react';
 import { usePage } from "@inertiajs/inertia-react";
 import AICanvas, { undoLastMove } from "@/Components/Student/AICanvas/AICanvas";
 
 import "../../../css/pages/Students/Level.css";
 
+function done() {
+    console.log("LOG");
+}
+
 export default function Sandbox() {
     const { level } = usePage().props;
+
+    const [drawing, setDrawing] = useState({
+        image: "",
+        assignment_id: level.id,
+        student_id: "",
+    });
+
     return (
         <article className="level__container">
             <Head title={level.name} />
@@ -14,7 +25,8 @@ export default function Sandbox() {
             <section className="canvas__container">
                 <AICanvas id="canvas" mode="level" prompt="vogel" />
             </section>
-            <button className="u__z_index2" onClick={undoLastMove}>Click me</button>
+            {/* <button className="u__z_index2" onClick={undoLastMove}>undoLastMove</button>
+            <button className="u__z_index2" onClick={done}>done</button> */}
         </article>
     )
 

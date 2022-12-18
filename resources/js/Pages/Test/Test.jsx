149711856310cf22e1e4e6dbb25a7758a65f5f3b
Kasper <kaspertrouwee@gmail.com>
2022-12-18 12:19:34 +0100

    ✨ Added the component with customization

--- resources/js/Pages/Test/Test.jsx
+++ resources/js/Pages/Test/Test.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextInput from '@/Components/TextInput';
 import CustomSecundaryButton from '@/Components/CustomSecundaryButton';
 import PrimaryButton from '@/Components/PrimaryButton';
-
+import Modal from '@/Components/Modal';
 
 export default function Test() {
+    const [modelState, setModelState] = useState(false);
     return (
         <section>
             <h1>Supertitle</h1>
@@ -20,6 +21,19 @@ export default function Test() {
             <br />
             <br />
             <TextInput placeholder={'username'} />
+            <br />
+            <br />
+            <button onClick={() => setModelState(true)}>Open Modal</button>
+            <Modal content={
+            <React.Fragment>
+                <div style={{width: '60vw', height: '40vw', backgroundColor: "var(--color-blue)", zIndex: "50"}}>
+                    <h3>Hello</h3>
+                    <p>Dit gebruikt een fragment</p>
+                    <p>Er zit zelfs custom styling op en een css var!</p>
+                    <marquee>Dit is super cool!</marquee>
+                    <button onClick={() => setModelState(false)}>Sluiten</button>
+                </div>
+            </React.Fragment>} modelState={modelState} setModelState = {setModelState}></Modal>
         </section>
         
     );

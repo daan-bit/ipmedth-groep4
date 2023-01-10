dbb013e0bfe6d4c2492875df903af9fe7ce6b3f3
Kasper <kaspertrouwee@gmail.com>
2023-01-10 16:24:47 +0100

    📱 Design is more responsive

--- resources/js/Pages/Worlds/World.jsx
+++ resources/js/Pages/Worlds/World.jsx
@@ -1,7 +1,6 @@
 import Navbar from "@/Components/World/NavBar";
 import WorldRoute from "@/Components/World/WorldRoute";
 import { Head } from "@inertiajs/inertia-react";
-import { abs } from "@tensorflow/tfjs";
 import React, { useState, useEffect } from "react";
 import "../../../css/pages/worldoverview.css";
 
@@ -18,30 +17,51 @@ function drawLine() {
         dictIslands[index] = { x: centerX, y: centerY };
     }
 
-    //loop through the dict minus one beacuse the line needs to connect 2 islands 
-    for (let index = 0; index < Object.keys(dictIslands).length-1; index++) {
-
+    //loop through the dict minus one beacuse the line needs to connect 2 islands
+    for (let index = 0; index < Object.keys(dictIslands).length - 1; index++) {
         //Checks if the index is even (al even islands are below)
         let indexEven = false;
-        if (index % 2 == 0) indexEven = true; 
+        if (index % 2 == 0) indexEven = true;
 
-        //calculate the width and height between the 2 center points (x and y) of 2 islands 
+        //calculate the width and height between the 2 center points (x and y) of 2 islands
         const width = Math.abs(dictIslands[index].x - dictIslands[index + 1].x);
-        const height = Math.abs(dictIslands[index].y - dictIslands[index + 1].y);
+        const height = Math.abs(
+            dictIslands[index].y - dictIslands[index + 1].y
+        );
 
         //create svg and add styling
         let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-        
+        svg.setAttributeNS(
+            "http://www.w3.org/2000/xmlns/",
+            "xmlns:xlink",
+            "http://www.w3.org/1999/xlink"
+        );
+
         // if indexeven gets the height of the second card, the one above
-        if (indexEven) svg.setAttribute( "style", `position: absolute; top: ${dictIslands[index].y}px; left: ${dictIslands[index].x}px; z-index:1;`);
-        else svg.setAttribute( "style", `position: absolute; top: ${dictIslands[index+1].y}px; left: ${dictIslands[index].x}px; z-index:1;`);
-        
+        if (indexEven)
+            svg.setAttribute(
+                "style",
+                `position: absolute; top: ${
+                    dictIslands[index].y
+                }px; left: ${dictIslands[index].x}px; z-index:1;`
+            );
+        else
+            svg.setAttribute(
+                "style",
+                `position: absolute; top: ${
+                    dictIslands[index + 1].y
+                }px; left: ${dictIslands[index].x}px; z-index:1;`
+            );
+
         svg.setAttribute("width", width);
         svg.setAttribute("height", height);
+        svg.setAttribute("class", "dashed-lines");
 
         //create line and add styling
-        let newLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        let newLine = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "line"
+        );
         newLine.setAttribute("id", "line2");
         newLine.setAttribute("x1", 0);
         newLine.setAttribute("x2", width);
@@ -63,6 +83,15 @@ function drawLine() {
     }
 }
 
+//Removes and readds the dashed lines for the new screen size
+window.addEventListener("resize", function () {
+    const dashed_lines = document.getElementsByClassName("dashed-lines");
+    while(dashed_lines.length > 0){
+        dashed_lines[0].parentNode.removeChild(dashed_lines[0]);
+    }
+    drawLine();
+});
+
 function World(props) {
     const world = props.world;
     const assignments = props.assignments;
@@ -76,9 +105,10 @@ function World(props) {
         );
         drawLine();
     }, [setCurrentLevel]);
+
     return (
         <article className="world">
-            <Head title="🏖️ Wereld" />
+            <Head title="🌎 Wereld 1" />
             <Navbar student_id={props.student.id} />
             <section className="world__route" id="world">
                 {world.map((item, key) => (

7106d1b9c9176ddc583199763717af48724694a7
Kasper <kaspertrouwee@gmail.com>
2023-01-11 11:42:51 +0100

    Build stuff

---
+++ public/build/assets/InputError.649dd925.js
@@ -0,0 +1,1 @@
+import{a as o}from"./app.b6cd464c.js";function l({message:r,className:t=""}){return r?o("p",{className:"error small_text",children:r}):null}export{l as I};
---
+++ public/build/assets/InputLabel.71c505dd.js
@@ -0,0 +1,1 @@
+import{a as l}from"./app.b6cd464c.js";function r({forInput:t,value:a,className:n,children:e}){return l("label",{htmlFor:t,className:"inputLabel",children:a||e})}export{r as I};

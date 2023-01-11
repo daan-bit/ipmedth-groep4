ce1eb5de8f209b52bb57d7ab187effb6220ff03e
Tycho van Veen <dev@tychovanveen.nl>
2023-01-11 10:56:49 +0100

    add build folder

---
+++ public/build/assets/InputLabel.b4686d24.js
@@ -0,0 +1,1 @@
+import{a as l}from"./app.21369ce3.js";function r({forInput:t,value:a,className:n,children:e}){return l("label",{htmlFor:t,className:"inputLabel",children:a||e})}export{r as I};
---
+++ public/build/assets/PrimaryButton.b6b4a7da.js
@@ -0,0 +1,1 @@
+import{a as m}from"./app.21369ce3.js";/* empty css               */function n({type:r="submit",className:a="button button-primary",processing:t,children:o}){return m("button",{type:r,className:` ${t} `+a,disabled:t,children:o})}export{n as P};
